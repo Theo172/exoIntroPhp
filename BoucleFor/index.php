a2ca7486cb9aa7fd7e918dd0ac5f707ae4bf0eed
Théo <t.ounoughi@gmail.com>
2021-11-04 16:51:48 +0100

    Abandon

---
+++ BoucleFor/index.php
@@ -0,0 +1,15 @@
+<?php
+$nb=0;
+while($nb<=25){
+    echo $nb ."<br>";
+    $nb++;
+    
+}
+
+for($n=25;$n>0;$n--){
+    echo $n ."<br>";
+}
+
+for($i=1;$i<26;$i++){
+    echo $i .'<br>';
+}
---
+++ Somme/index.php
@@ -0,0 +1,11 @@
+<?php
+
+function somme($a,$b){
+
+    while($a<$b){
+        
+        $a++
+    }
+    return $a+$a[$a-1]
+}
+   echo somme(1,30)
